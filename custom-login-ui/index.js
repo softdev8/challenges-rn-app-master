ad69b2864abba5f4320ee229347278487f1a9582
softdev9 <aprildev86@gmail.com>
2020-02-26 11:06:26 +0200

    custom login ui

---
+++ custom-login-ui/index.js
@@ -0,0 +1,11 @@
+import Loading from './Loading';
+import SignIn from './SignIn';
+import ConfirmSignIn from './ConfirmSignIn';
+import SignUp from './SignUp';
+import ConfirmSignUp from './ConfirmSignUp';
+import ForgotPassword from './ForgotPassword';
+import RequireNewPassword from './RequireNewPassword';
+import VerifyContact from './VerifyContact';
+
+
+export { SignIn, ConfirmSignIn, SignUp, ConfirmSignUp, ForgotPassword, Loading, RequireNewPassword, VerifyContact };
